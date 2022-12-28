bc50ebe8bce6250f3c9bd53a5641d1aaf16dfa91
ansunny1170 <ansunny1170@gmail.com>
2022-12-28 10:39:36 +0900

    [ADD:zap:] commit calculator code

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import YechankimCalculator from '@/yechankim-calculator.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    YechankimCalculator
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <yechankim-calculator />
+  </div>
+</template>
